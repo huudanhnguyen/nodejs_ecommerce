862b61611226ec7df9609d3c0cd0bb0af9a1c1e8
DESKTOP-UKD0218\huuda <huudanh2001.hd@gmail.com>
2022-11-14 23:45:14 +0700

    wishlist and cart

--- app/routes/frontend/search.js
+++ app/routes/frontend/search.js
@@ -6,31 +6,15 @@ const productsModel 		= require(__path_schemas + col_products);
 const settingsModel 		= require(__path_schemas + col_settings);
 const sliderModel 		= require(__path_schemas + col_sliders);
 const categoryModel 		= require(__path_schemas + col_categories);
+const ParamsHelpers = require(__path_helpers + "params");
 const Model 		= require(__path_models + 'products');
 const menuModel 		= require(__path_schemas + col_menu);
 const folderView	 = __path_view_ecommerce + 'pages/search/';
 const layout	     = __path_view_ecommerce+ 'frontend';
+const UtilsHelpers 	= require(__path_helpers + 'utils');
 /* GET home page. */
-router.get('/',async (req, res, next) => {
-  // let itemsSpecial 	= [];
-  // await Model.listItemsFrontend(null, {task: 'items-special'} ).then( (items) => { itemsSpecial = items;
-  //   items.map(async i=>{
-  //     let{name}=await categoryModel.findById((i.categoriesId))
-  //       i.categoryName=name;
-  //       return i
-  //     })
-  //     items= itemsSpecial;
-  // });
-  // await Model.listItemsFrontend(null, {task: 'items-ecommerce'} ).then( (items) => { itemsEcommerce = items;
-  //   items.map(async i=>{
-  //     let{name}=await categoryModel.findById((i.categoriesId))
-  //       i.categoryName=name;
-  //       return i
-  //     })
-  //     itemsEcommerce=items; 
-  // });
-  let keywordData = req.query.keyword
-  let keyword = new RegExp(keywordData, 'i')
+router.get('/(:slug)?',async (req, res, next) => {
+
   let item = await settingsModel.findOne({});
 
 	const {copyright, content, logoFooter,phoneFooter,email,address,linkfacebook,linkyoutube} = JSON.parse(item.footer);
@@ -48,23 +32,36 @@ router.get('/',async (req, res, next) => {
 	item.phoneHeader = phoneHeader;
 	item.notification = notification;
 	item.logoHeader = logoHeader;
+  let filterPrice       = (req.query.minPrice && req.query.maxPrice) ?[
+    {price: {$gte: req.query.minPrice}},
+    {price: {$lte: req.query.maxPrice}}
+  ] :  undefined
+  let sortPrice     = {}
+  if(req.query.sort){
+      let dataSort      = req.query.sort.split(",")
+      sortPrice[dataSort[0]]     = ((dataSort[0] == 'price') && (dataSort[1] =='asc' || dataSort[1] =='desc'))? dataSort[1] : ''
+  }
 
-  const listProducts = await productsModel.find({name: {$regex: keyword}});
-  const listProductBestSeller = await productsModel.find({bestseller:true,status:'active', name: {$regex: keyword}});
-  const listProductNewArrivals = await productsModel.find({newarrivals:true,status:'active', name: {$regex: keyword}});
+  let queryPrice = (req.query.minPrice && req.query.maxPrice) ? filterPrice : {}
+
+  const listProductNewArrivals = await productsModel.find({newarrivals:true,status:'active'});
   const listMenu = await menuModel.find({status:'active'}).sort({ordering: 'desc'});
   const listSliders = await sliderModel.find({status:'active'});
   const listCategory = await categoryModel.find({}).sort({ ordering: "desc" });
+  let objWhere={status:'active'}
+  if(filterPrice){
+    objWhere.$and= filterPrice
+  }
+  const listProducts = await productsModel.find(objWhere).limit().sort(sortPrice);
   res.render(`${folderView}index`, { 
     layout,
     listProducts,
     listSliders,
     listMenu,
     item,
-    listProductBestSeller,
     listProductNewArrivals,
     listCategory,
-    keywordData,
+    queryPrice,
     slider:false
   });
 });
